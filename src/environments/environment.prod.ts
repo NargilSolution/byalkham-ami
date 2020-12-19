c058185e497d87f8da3e3c7e0850ba0f445ca5e6
uuree03 <uuree03@gmail.com>
2020-12-19 12:06:35 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDJMZvjzqhLBQbZXm3BUxzchQ6xgs0hITw",
+    authDomain: "byalkham-ami.firebaseapp.com",
+    projectId: "byalkham-ami",
+    storageBucket: "byalkham-ami.appspot.com",
+    messagingSenderId: "971772878460",
+    appId: "1:971772878460:web:cfb77aa975a9a8c79f33f9",
+    measurementId: "G-VT6P125LXT"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDJMZvjzqhLBQbZXm3BUxzchQ6xgs0hITw",
+    authDomain: "byalkham-ami.firebaseapp.com",
+    projectId: "byalkham-ami",
+    storageBucket: "byalkham-ami.appspot.com",
+    messagingSenderId: "971772878460",
+    appId: "1:971772878460:web:cfb77aa975a9a8c79f33f9",
+    measurementId: "G-VT6P125LXT"
+  }
 };
 
 /*
